c2b2fd20c5b282737f1795aae5afcea3bc524def
AbdulYusuf <abdul17yusuf@gmail.com>
2024-09-28 10:01:39 +0100

    update

--- CleanUpBackend/settings.py
+++ CleanUpBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!1m&-*)6)ifc)jh3do_v6zy$!=yjqw#ruu6x51okmp#qd=udu(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['cleanupbackend-production.up.railway.app', 'railway.app']
 
 
 # Application definition
